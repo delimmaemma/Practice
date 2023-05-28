800d5cc4ee1befe292371fc2574907797e2211e4
delimmaemma <calvinhtml@gmail.com>
2023-05-27 21:57:24 -0500

    Commented out 90 questions to provide a sample quiz.

---
+++ quiz.js
@@ -0,0 +1,822 @@
+var myQuestions = [
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    },
+    {
+        question: "Is this:",
+        answers: {
+            'Jiji',
+            'Nugget'
+        },
+        correctAnswer:
+    }
+];
+function generateQuiz(questions, quizContainer, resultsContainer, previousButton, nextButton, submitButton) {
+    function showQuestions(questions, quizContainer) {
+
+    }
+}
+
+function showResults(questions, quizContainer, resultsContainer) {
+
+}
+
+showQuestions(questions, quizContainer);
+submitButton.onclick = function() {
+    showResults(questions, quizContainer, resultsContainer)
+}
+previousButton.onclick = function() {
+
+}
+nextButton.onclick = function() {
+    
+}
